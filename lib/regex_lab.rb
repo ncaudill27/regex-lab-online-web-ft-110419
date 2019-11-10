b578cd2a5db2ccb26940fb781cc796dae3de70de
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-10 18:56:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  textmatch(/^(un ing)$/) ? true : false
+  textmatch(/^(un..ing)$/) ? true : false
 end
 
 def words_five_letters_long(text)
